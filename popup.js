726c3af66db72786292c78d73dd517d242ace150
yobukodori <32874862+yobukodori@users.noreply.github.com>
2020-10-22 22:26:43 +0900

    v.0.1.6

--- popup.js
+++ popup.js
@@ -1,20 +1,42 @@
 function onDOMContentLoaded(platformInfo){
 	let os = platformInfo.os, is_mobile = os === "android", is_pc = ! is_mobile;
-	console.log("is_pc:",is_pc,"is_mobile:",is_mobile);
+	function error(msg){
+		let e = document.createElement("div");
+		e.appendChild(document.createTextNode("Error: " + msg));
+		document.body.insertBefore(e, document.body.firstChild);
+	}
 	document.querySelector('#enable-future').addEventListener('change', ev=>{
 		browser.runtime.sendMessage({type: "toggle"});
 	});
 	document.querySelector('#settings').addEventListener('click', ev=>{
-		browser.runtime.openOptionsPage()
-		.then(()=>{
-			if (is_pc){
-				window.close();
-			}
-		})
-		.catch(err=>{
-			document.body.insertBefore(
-				document.createTextNode("Error: " + err), document.body.firstChild);
-		});
+		if (is_mobile){
+			let url = browser.runtime.getURL("options.html");
+			browser.tabs.query({})
+			.then(tabs=>{
+				let found;
+				for (let i = 0 ; i < tabs.length ; i++){
+					let tab = tabs[i];
+					if (tab.url === url){
+						found = true;
+						browser.tabs.update(tab.id, {active: true})
+						.then(tab=>{ window.close(); })
+						.catch(error);
+						break;
+					}
+				}
+				if (! found){
+					browser.tabs.create({url: url})
+					.then(tab=>{ window.close(); })
+					.catch(error);
+				}
+			})
+			.catch(error);
+		}
+		else {
+			browser.runtime.openOptionsPage()
+			.then(()=>{ window.close(); })
+			.catch(error);
+		}
 	});
 
 	document.querySelectorAll("body, input, textarea, button").forEach(e=>{
@@ -30,9 +52,7 @@ function onDOMContentLoaded(platformInfo){
 			document.querySelector('label[for="enable-future"]').classList.add("gray");
 		}
 	})
-	.catch (err=>{
-		document.querySelector(".main").textContent = "Error getStatus:" + err;
-	});
+	.catch(error);
 }
 
 document.addEventListener('DOMContentLoaded', ev=>{
